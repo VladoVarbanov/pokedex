0ef756d49a85fc850a5f1ac3b6d3bc2d49fd6318
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-25 19:32:43 +0200

    Create SideNav.jsx

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { first151Pokemon, getFullPokedexNumber } from "../utils";
+
+export default function SideNav() {
+  return (
+    <nav>
+      <div className={"header"}>
+        <h1 className="text-gradient">Pokédex</h1>
+      </div>
+      <input type="text" />
+      {first151Pokemon.map((pokemon, pokemonIndex) => {
+        return (
+          <button key={pokemonIndex} className={"nav-card "}>
+            <p>{getFullPokedexNumber(pokemonIndex)}</p>
+            <p>{pokemon}</p>
+          </button>
+        );
+      })}
+    </nav>
+  );
+}
