91271693e0997afe35fd0e2839494c9867811cd7
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-26 20:09:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
+import Header from "./components/Header.jsx";
+import SideNav from "./components/SideNav.jsx";
+import PokeCard from "./components/PokeCard.jsx";
 
 function App() {
-  return <></>;
+  const [selectedPokemon, setSelectedPokemon] = useState(0);
+  return (
+    <>
+      <Header selectedPokemon={selectedPokemon} />
+      <SideNav
+        selectedPokemon={selectedPokemon}
+        setSelectedPokemon={setSelectedPokemon}
+      />
+      <PokeCard selectedPokemon={selectedPokemon} />
+    </>
+  );
 }
 
 export default App;
